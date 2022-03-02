71407038858a850312db025a9099410916fe7722
Vic van Gool <lvangool@users.noreply.github.com>
2022-03-02 11:00:31 +0000

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,8 @@ module Rails702
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.active_record.schema_format = :sql
+    
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
